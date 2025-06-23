f71d3c4263187ff2dcd69f055f2aa7bb4f68f306
BaltasisKos <Baltasiskostas@hotmail.com>
2025-06-24 00:59:18 +0300

    feat:header component for dashboard

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { HiOutlineSearch, HiOutlineChatAlt, HiOutlineBell } from 'react-icons/hi'
+
+const Header = () => {
+  return (
+    <div className='bg-white h-16 w-auto px-4 flex justify-between items-center border-b border-gray-200'>
+      <div className='relative'>
+        <HiOutlineSearch fontSize={20} className='text-gray-400 absolute top-1/2 -translate-y-1/2 left-3'/>
+        <input type="text" placeholder='Search...' className='text-sm focus:outline-none active:outline-none h-10 w-120 border border-gray-300 rounded-lg pl-11 pr-4'/>
+      </div>
+      <div className='flex items-center gap-2 mr-2'>
+        
+
+        <HiOutlineChatAlt fontSize={24}/>
+
+
+
+
+        <HiOutlineBell fontSize={24}/>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
 
 const Dashboard = () => {
   return (
     <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
       <Sidebar/>
-      <div className='p-4'>
-      <div className='bg-teal-200'>Header</div>
+      <div className='flex-1 h-screen w-screen'>
+      <Header/>
       </div>
     </div>
   )
